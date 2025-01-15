3b373beeb5ad8f2861191eca55565a0cdd0b14f6
Debottam Kar <debottamkar2003@gmail.com>
2025-01-15 20:32:29 +0530

    database

--- pg_connect.js
+++ pg_connect.js
@@ -6,7 +6,14 @@ let pg_obj = new Pool({
     host : "localhost",
     database : "student",
     password : process.env.DB_PASSWORD,
-    port : process.env.DB_PORT
+    port : process.env.DB_PORT,
+    max : 20
 });
+
+async function getdata(){
+let data = await pg_obj.query("select * from todo");
+console.log(data.rows);
+}
+getdata();
 
 module.exports = pg_obj;
